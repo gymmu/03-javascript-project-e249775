9c0e31f19daab6c1c003b79024d977dae7cf6565
Lucy Wehrli <e249775@sbl.ch>
2023-10-30 11:44:05 +0100

    aufgabe 1 und 2 komplett

--- script.js
+++ script.js
@@ -1,10 +1,61 @@
-export function aufgabe01(args) {
+
+
+  export function aufgabe01(args) {
+    const input = args
+    const result = []
+   
+    for (let i = 0; i < input.length; i++) {
+      const currentElement = input[i]
+   // Wenn das aktuelle Zeichen ein 'e' ist, hänge nichts an die
+  if (currentElement === 'e') {
+    result.push("")
+    // Wenn das aktuelle Zeichen 'E' ist, hänge nichts an die
+  }else if ( currentElement === 'E'){
+   
+  }else {
+    result.push(currentElement)
+  }
+   
+    }
+    return result.join("")
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export function aufgabe02(args) {
   const input = args
   const result = []
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+    const capitalizedElement = currentElement.toUpperCase ()
+    result.push (capitalizedElement)
+
   }
   return result.join("")
 }
+
+
