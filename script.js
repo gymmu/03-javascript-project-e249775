5eff72f83a2f97baf7f6c7ff53063801be898113
Lucy Wehrli <e249775@sbl.ch>
2023-10-31 09:27:29 +0100

    ende letion 31

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+// Aufgabe 1
   export function aufgabe01(args) {
     const input = args
     const result = []
@@ -20,7 +20,7 @@
     return result.join("")
   }
 
-
+// Aufgabe 2
 
 export function aufgabe02(args) {
   const input = args
@@ -35,7 +35,7 @@ export function aufgabe02(args) {
   return result.join("")
 }
 
-
+// Aufgabe 3
 export function aufgabe03(args) {
   const input = args
   const result = []
@@ -45,26 +45,62 @@ let count = 0
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     //alle e sollen gelöscht werden
+    
     if (currentElement === 'e'){
       count = count + 1
-      
+      // zählt alle kleinen e
     }
     else if (currentElement === 'E')
     count = count +1
+  // zählt alle grossen E
   }
   return count
 }
 
-
-export function aufgabe02(args) {
+// Aufgabe 4
+export function aufgabe04(args) {
   const input = args
   const result = []
+let count = 0
+
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    const capitalizedElement = currentElement.toUpperCase ()
-    result.push (capitalizedElement)
-
+    
+    if (currentElement === 'e'){
+      count = count + 1
+     
+    }
+    else if (currentElement.number)
+    count = count +1
+ 
   }
-  return result.join("")
+  return count
+}
+
+// Aufgabe 5
+
+export function aufgabe05(args) {
+  const input = args
+  const result = []
+  let capitalLetters = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input [i]
+    if (currentElement === ".") {
+
+    } else if (currentElement === " ") {
+
+    }
+    else if (currentElement === currentElement.toUpperCase()) {
+
+    }
+  }
+
+  if (capitalLetters> 0) {
+    return true
+  }else {
+    return false
+  }
+  
 }
