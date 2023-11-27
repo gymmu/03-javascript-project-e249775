610ad5c3214a55f1112290cc82af3989fce11020
Lucy Wehrli <e249775@sbl.ch>
2023-11-27 11:52:52 +0100

    aufgabe 20

--- script.js
+++ script.js
@@ -244,3 +244,19 @@ export function aufgabe07(args) {
   return false
 }
   
+// AUfgabe 20
+
+export function aufgabe20 (args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === '.') {
+      if (input[i+1] === ' ') {
+          return true
+        }
+      }
+    } 
+  return false
+  }
+  
